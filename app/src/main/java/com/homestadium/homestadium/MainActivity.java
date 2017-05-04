3e39b677550f2997ea908b6fe99ee1cb5cb726dc
Kevin Anin <kbacreation@gmail.com>
2017-05-04 14:16:29 +0200

    created main activity and started login activity

--- app/src/main/java/com/homestadium/homestadium/MainActivity.java
+++ app/src/main/java/com/homestadium/homestadium/MainActivity.java
@@ -1,13 +1,32 @@
 package com.homestadium.homestadium;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
+
+    public void loginActivity(View view){
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+
+
+
+
 }
